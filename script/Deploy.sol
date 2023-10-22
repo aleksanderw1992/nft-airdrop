15363a75e1b4b9857740d88aa459e64f89b8a971
Aleksander Wojcik <aleksander.w1992@gmail.com>
2023-10-22 21:17:19 +0200

    Try deploy

--- script/Deploy.sol
+++ script/Deploy.sol
@@ -3,7 +3,31 @@ pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
 import "../src/BetcometsErc1155.sol";
+///@notice Script to deploy contracts to local blockchain
+/*
+create .env file with your environments:
+pk=...
+MUMBAI_RPC_URL=...
+SEPOLIA_RPC_URL=...
+POLYGON_RPC_URL=...
+SEPOLIA_API_KEY
 
+For local deployment use commands:
+anvil
+// pick from there private key and public key
+export pk=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+forge script ./script/Deploy.sol:Deploy --fork-url http://localhost:8545 --private-key $pk --broadcast --resume
+
+--- Deployment on testnet:
+source .env
+#forge script ./script/Deploy.sol:Deploy --fork-url $SEPOLIA_RPC_URL --broadcast --verify -vvvv
+#forge create --rpc-url $SEPOLIA_RPC_URL --private-key $pk ./src/BetcometsErc1155.sol:BetcometsErc1155
+forge create --rpc-url $SEPOLIA_RPC_URL --private-key $pk --etherscan-api-key $SEPOLIA_API_KEY --verify ./src/BetcometsErc1155.sol:BetcometsErc1155
+
+verify cast is working:
+cast call 0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0 "uri()"
+
+*/
 contract Deploy is Script {
     function setUp() public {}
 
--- src/BetcometsErc1155.sol
+++ src/BetcometsErc1155.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
+
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
