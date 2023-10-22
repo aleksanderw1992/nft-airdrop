e221d78626644fe9eaff6db23f5b09eadaa89759
Aleksander Wojcik <aleksander.w1992@gmail.com>
2023-10-22 21:58:36 +0200

    Deploy on mainnet

--- script/Deploy.sol
+++ script/Deploy.sol
@@ -7,10 +7,11 @@ import "../src/BetcometsErc1155.sol";
 /*
 create .env file with your environments:
 pk=...
-MUMBAI_RPC_URL=...
-SEPOLIA_RPC_URL=...
-POLYGON_RPC_URL=...
-SEPOLIA_API_KEY
+MUMBAI_RPC_URL=
+SEPOLIA_RPC_URL=
+SEPOLIA_API_KEY=
+POLYGON_RPC_URL=
+POLYGON_API_KEY=
 
 For local deployment use commands:
 anvil
@@ -23,6 +24,7 @@ source .env
 #forge script ./script/Deploy.sol:Deploy --fork-url $SEPOLIA_RPC_URL --broadcast --verify -vvvv
 #forge create --rpc-url $SEPOLIA_RPC_URL --private-key $pk ./src/BetcometsErc1155.sol:BetcometsErc1155
 forge create --rpc-url $SEPOLIA_RPC_URL --private-key $pk --etherscan-api-key $SEPOLIA_API_KEY --verify ./src/BetcometsErc1155.sol:BetcometsErc1155
+forge create --rpc-url $POLYGON_RPC_URL --private-key $pk --etherscan-api-key $POLYGON_API_KEY --verify ./src/BetcometsErc1155.sol:BetcometsErc1155
 
 verify cast is working:
 cast call 0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0 "uri()"
